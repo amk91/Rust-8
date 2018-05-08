f04cb6fd8438f28c959285ab2d76c41588840e13
amk91 <agostino.pisanelli@gmail.com>
2018-05-08 23:21:46 +0200

    Added missing functions. Added shared variables for delay and sound timers.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,18 @@
-use std::{thread, time};
+use std::{thread, time::Duration, sync::Arc, sync::RwLock,
+		env, fs::File,
+		io, io::Read, io::Write,
+		prelude::*};
 
 extern crate rand;
 use rand::{Rng, thread_rng};
 
 extern crate termion;
 use termion::raw::IntoRawMode;
-use std::io::{Write, stdout, stdin};
+
+extern crate termios;
+use termios::{Termios, TCSANOW, ECHO, ICANON, tcsetattr};
+
+extern crate adi_clock;
 
 const N_REGISTERS: usize = 16;
 const N_STACK: usize = 32;
@@ -234,22 +241,48 @@ fn drw(vx: usize, vy: usize, bytes_number: u8,
 	}
 }
 
-//TODO: define SKP and SKPN
-
-fn ld_delay_to_reg(vx: usize, delay_timer: &u8, V: &mut Registers)
+fn skp(vx: usize, key_pressed: u8,
+	V: &Registers, pc: &mut u16)
 {
+	if V[vx] == key_pressed
+	{
+		*pc += 2;
+	}
+}
+
+fn sknp(vx: usize, key_pressed: u8,
+	V: &Registers, pc: &mut u16)
+{
+	if V[vx] != key_pressed
+	{
+		*pc += 2;
+	}
+}
+
+fn ld_delay_to_reg(vx: usize, delay_timer: Arc<RwLock<u8>>, V: &mut Registers)
+{
+	let delay_timer = delay_timer.read().unwrap();
 	V[0] = *delay_timer;
 }
 
-//TODO: define LD_KEY
-
-fn ld_reg_to_delay(vx: usize, delay_timer: &mut u8, V: &Registers)
+fn ld_key(vx: usize, V: &mut Registers,
+	stdin: &mut io::Stdin, stdout: &io::Stdout)
 {
+	let mut buffer : [u8; 1] = [0; 1];
+	stdout.lock().flush().unwrap();
+	stdin.read_exact(&mut buffer).unwrap();
+	V[vx] = buffer[0];
+}
+
+fn ld_reg_to_delay(vx: usize, delay_timer: Arc<RwLock<u8>>, V: &Registers)
+{
+	let mut delay_timer = delay_timer.write().unwrap();
 	*delay_timer = V[vx];
 }
 
-fn ld_reg_to_sound(vx: usize, sound_timer: &mut u8, V: &Registers)
+fn ld_reg_to_sound(vx: usize, sound_timer: Arc<RwLock<u8>>, V: &Registers)
 {
+	let mut sound_timer = sound_timer.write().unwrap();
 	*sound_timer = V[vx];
 }
 
@@ -258,7 +291,32 @@ fn add_reg_index(vx: usize, I: &mut u16, V: &Registers)
 	*I += V[vx] as u16;
 }
 
-fn get_op_code(memory: &Memory, pc: &mut u16) -> OpCode
+fn set_sprite(vx: usize, V: &Registers, I: &mut u16)
+{
+	if V[vx] >= 0 && V[vx] <= 9
+	{
+		*I = V[vx] as u16;
+	}
+	else if V[vx] >= 'A'.to_digit(10).unwrap() as u8 &&
+		V[vx] <= 'F'.to_digit(10).unwrap() as u8
+	{
+		*I = V[vx] as u16;
+	}
+}
+
+fn store_bcd(vx: usize, V: &Registers,
+	I: &u16, memory: &mut Memory)
+{
+	let h = V[vx] / 100 & 0b00000111;
+	let d = V[vx] / 10 & 0b00000111;
+	let u = V[vx] & 0b00000111;
+
+	memory[*I as usize] = h;
+	memory[(*I + 1) as usize] = d;
+	memory[(*I + 2) as usize] = u;
+}
+
+fn get_op_code(memory: &Memory, pc: &u16) -> OpCode
 {
 	let mut op_code: OpCode = OpCode::default();
 	op_code.code = memory[*pc as usize] >> 4;
@@ -305,11 +363,69 @@ fn get_op_code(memory: &Memory, pc: &mut u16) -> OpCode
 		_ => { },
 	};
 
-	*pc += 2;
 	op_code
 }
 
+fn load_rom(filepath: String, memory: &mut Memory)
+{
+	let mut file = File::open(filepath).expect("File not found");
+	let mut buffer: Vec<u8> = Vec::new();
+	file.read_to_end(&mut buffer);
 
+	let mut index = 0x200;
+	for byte in buffer.iter()
+	{
+		if index < 4096
+		{
+			memory[index] = *byte;
+			index += 1;
+		}
+	}
+}
+
+fn tick_60hz(delay_timer: Arc<RwLock<u8>>,
+	sound_timer: Arc<RwLock<u8>>)
+{
+	loop
+	{
+		{
+			let mut delay_timer = delay_timer.write().unwrap();
+			if *delay_timer > 0
+			{
+				*delay_timer -= 1;
+			}
+		}
+
+		{
+			let mut sound_timer = sound_timer.write().unwrap();
+			if *sound_timer > 0
+			{
+				*sound_timer -= 1;
+				//TODO: sound the buzzer
+			}
+		}
+
+		thread::sleep(Duration::from_millis(17));
+	}
+}
+
+fn draw(frame_buffer: &FrameBuffer, stdout: &io::Stdout)
+{
+    let mut stdout = stdout.lock().into_raw_mode().unwrap();
+	for i in 0..frame_buffer.len()
+	{
+		for j in 0..frame_buffer[i].len()
+		{
+			let mut c = " ";
+			if !frame_buffer[i][j]
+			{
+				c = "▓";
+			}
+
+			write!(stdout, "{}{}", termion::cursor::Goto(i as u16 + 1, j as u16 + 1), c).unwrap();
+		}
+	}
+}
 
 fn main()
 {
@@ -322,30 +438,28 @@ fn main()
 	let mut frame_buffer: FrameBuffer = [[false; N_FRAMEBUFFER_HEIGHT]; N_FRAMEBUFFER_WIDTH];
 	let mut memory: Memory = [0; N_MEMORY];
 
-	let mut delay_timer: u8 = 0;
-	let mut sound_timer: u8 = 0;
 	let mut pc: u16 = 0x200;
 
-	let cycle_time = time::Duration::from_millis(16);
+	// Init termios and termion data
+	let stdin = 0;
+	let termios = Termios::from_fd(stdin).unwrap();
+	let mut new_termios = termios.clone();
+	new_termios.c_lflag &= !(ICANON | ECHO);
+	tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
+	let stdout = io::stdout();
+	let mut reader = io::stdin();
 
-	let stdout = stdout();
-    let mut stdout = stdout.lock().into_raw_mode().unwrap();
-    let stdin = stdin();
-	let stdin = stdin.lock();
+	let delay_timer = Arc::new(RwLock::new(0 as u8));
+	let shared_delay_timer = delay_timer.clone();
 
-	write!(stdout, "{}", termion::clear::All);
+	let sound_timer = Arc::new(RwLock::new(0 as u8));
+	let shared_sound_timer = sound_timer.clone();
 
-	for i in 0..frame_buffer.len()
+	thread::spawn(|| tick_60hz(shared_delay_timer, shared_sound_timer));
+
+	loop
 	{
-		for j in 0..frame_buffer[i].len()
-		{
-			write!(stdout, "{}▓", termion::cursor::Goto(i as u16 + 1, j as u16 + 1)).unwrap();
-		}
-	}
-
-	/*loop
-	{
-		let op = get_op_code(&memory, &mut pc);
+		let op = get_op_code(&memory, &pc);
 		if op.x.is_none() || op.y.is_none() || op.variant.is_none()
 		{
 			//TODO: raise error
@@ -363,6 +477,7 @@ fn main()
 					{
 						0x0 => cls(&mut frame_buffer),
 						0xE => ret(&mut pc, &mut sp, &stack),
+
 						_ =>
 						{
 							//TODO: raise error
@@ -389,6 +504,7 @@ fn main()
 						0x6 => shr(x as usize, &mut V),
 						0x7 => subn_regs(x as usize, y as usize, &mut V),
 						0xE => shl(x as usize, &mut V),
+
 						_ =>
 						{
 							//TODO: raise error
@@ -403,12 +519,11 @@ fn main()
 				{
 					match variant
 					{
-						0x07 => ld_delay_to_reg(x as usize, &delay_timer, &mut V),
-						0x15 => ld_reg_to_delay(x as usize, &mut delay_timer, &V),
-						0x18 => ld_reg_to_sound(x as usize, &mut sound_timer, &V),
+						0x07 => ld_delay_to_reg(x as usize, delay_timer.clone(), &mut V),
+						0x15 => ld_reg_to_delay(x as usize, delay_timer.clone(), &V),
+						0x18 => ld_reg_to_sound(x as usize, sound_timer.clone(), &V),
 						0x1E => add_reg_index(x as usize, &mut I, &V),
-						//TODO: call Fx29
-
+						
 
 						_ =>
 						{
@@ -422,6 +537,13 @@ fn main()
 
 				},
 			};
+
+			pc += 2;
 		}
-	}*/
+
+		thread::sleep(Duration::from_millis(2));
+	}
+
+	// Reset stdin to original termios data
+	tcsetattr(stdin, TCSANOW, & termios).unwrap();
 }
